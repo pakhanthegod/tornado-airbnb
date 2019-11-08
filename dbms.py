55441796e12a8f389c39729aea3e6f7fb19c57ab
Pavel Mametev <pavelmametev@yandex.ru>
2019-11-08 19:49:34 +0500

    Change logic of working db layer

--- dbms.py
+++ dbms.py
@@ -1,6 +1,11 @@
 import decimal
 import asyncio
+import os
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
 
+from aiopg.sa.connection import SAConnection
+from aiopg.sa.result import ResultProxy, RowProxy
 from aiopg.sa import create_engine
 
 from models.houses import houses
@@ -8,25 +13,40 @@ from models.orders import orders
 from models.users import users
 
 
-class DBMS:
+if TYPE_CHECKING:
+    from aiopg.sa.result import ResultProxy, RowProxy
+    from aiopg.sa.connection import SAConnection
+    from aiopg.utils import _PoolContextManager
+
+
+class DBMS(ABC):
     connection = None
-    user='pakhan'
-    database='airbnb'
-    host='0.0.0.0'
-    port='54320'
-    password='pakhan'
+    user = os.environ.get('DB_USER')
+    password = os.environ.get('DB_PASSWORD')
+    database = os.environ.get('DB_NAME')
+    host = os.environ.get('DB_HOST')
+    port = os.environ.get('DB_PORT')
 
     def __init__(self):
         pass
 
-    async def get_engine(self):
-        engine = await create_engine(
+    async def get_engine(self) -> '_PoolContextManager':
+        engine: '_PoolContextManager' = await create_engine(
             user=self.user, database=self.database, host=self.host,
             port=self.port, password=self.password
         )
         return engine
 
+    @abstractmethod
     async def insert(self):
+        pass
+
+    @abstractmethod
+    async def delete(self):
+        pass
+
+    @abstractmethod
+    async def update(self):
         pass
 
 
@@ -35,25 +55,20 @@ class HouseDBMS(DBMS):
         super().__init__()
         self.houses = houses
     
-    async def insert(self, **kwargs):
-        engine = await self.get_engine()
-        async with engine:
-            async with engine.acquire() as connection:
-                await connection.execute(self.houses.insert().values(**kwargs))
+    async def insert(self, connection: 'SAConnection', **kwargs) -> 'RowProxy':
+        result: 'ResultProxy' = await connection.execute(self.houses.insert().values(**kwargs))
+        return (await result.first())[0]
 
-    async def delete(self, id):
-        engine = await self.get_engine()
-        async with engine:
-            async with engine.acquire() as connection:
-                await connection.execute(self.houses.delete().where(houses.id==id))
+    async def delete(self, connection: 'SAConnection', id: int, **kwargs) -> 'RowProxy':
+        result: 'ResultProxy' = await connection.execute(self.houses.delete().where(houses.c.id==id))
 
-    async def update(self, id, **kwargs):
-        engine = await self.get_engine()
-        async with engine:
-            async with engine.acquire() as connection:
-                await connection.execute(self.houses.update().where(houses.id==id).values(**kwargs))
+    async def update(self, connection: 'SAConnection', id: int, **kwargs) -> None:
+        await connection.execute(self.houses.update().where(self.houses.c.id==id).values(**kwargs))
 
+    async def selectById(self, connection: 'SAConnection', id: int, **kwargs) -> 'RowProxy':
+        result: 'ResultProxy' = await connection.execute(self.houses.select().where(self.houses.c.id==id))
+        return await result.first()
 
-if __name__ == '__main__':
-    dbms = DBMS()
-    asyncio.run(dbms.insert())
+    async def selectAll(self, connection: 'SAConnection', **kwargs) -> 'RowProxy':
+        result: 'ResultProxy' = await connection.execute(self.houese.select())
+        return await result.fetchall()
